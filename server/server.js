a318b949ca750e3c17706a0332c94b30890585b9
Zahid Farooq <zfarooq@tams.com>
2018-07-17 14:08:38 +0500

    heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req, resp)=>{
@@ -45,8 +45,8 @@ app.get('/todos/:id',(req, resp)=>{
 },(err)=>{
     resp.status(400).send(err);
 })
-app.listen(3000,()=>{
-    console.log('Started at 3000');
+app.listen(port,()=>{
+    console.log(`Started at ${port}`);
 
 });
 
